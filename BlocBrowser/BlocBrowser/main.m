80c9f37b4ab81bd771f62c0927689ccccb1d7dd0
Aaron Brager <getaaron@gmail.com>
2014-06-18 16:55:56 -0400

    Building a Web Browser App

---
+++ BlocBrowser/BlocBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Aaron on 6/18/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLCAppDelegate class]));
+    }
+}
