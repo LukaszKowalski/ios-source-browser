089a3f1c832124bc11e785f30e8320ccf231f870
Lukasz Kowalski <lukasz.kowalski91@gmail.com>
2014-07-13 14:23:25 +0200

    Initial Commit

---
+++ BlocBrowser/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  BlocBrowser
+//
+//  Created by Łukasz Kowalski on 7/13/14.
+//  Copyright (c) 2014 Lukasz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlocBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Łukasz Kowalski on 7/13/14.
+//  Copyright (c) 2014 Lukasz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLCAppDelegate class]));
+    }
+}
